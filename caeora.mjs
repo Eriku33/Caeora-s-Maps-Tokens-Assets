b6986b6de7931b1bf9a73ab7f4720bfc91d52f3b
Eriku33 <47860010+Eriku33@users.noreply.github.com>
2020-12-24 20:38:00 +0000

    Removed CR requirement to make actors
    Monster NPCs can be made manually from the create actor button provided you input a correctly spelled name.

--- caeora.mjs
+++ caeora.mjs
@@ -1,8 +1,8 @@
 // The base token path
 const TOKEN_PATH = "modules/caeora-maps-tokens-assets/assets/tokens/";
 
-// A cached list of available tokens
-let availableTokens = new Set();
+// A cached dictionary of avalible tokens names
+let availableTokenNames = new Object();
 
 // Store a reference to whether artwork is being replaced
 let replaceArtwork = false;
@@ -25,9 +25,8 @@ function initialize() {
 		default: false,
 		onChange: replace => replaceArtwork = replace
 	});
-	
-	// Assign the current saved value if there is one
-        replaceArtwork = game.settings.get("caeora-maps-tokens-assets", "replaceArtwork") ?? false;
+
+	replaceArtwork = game.settings.get("caeora-maps-tokens-assets", "replaceArtwork")
 
 	// Handle actor replacement, if the setting is enabled
 	Hooks.on("preCreateActor", replaceActorArtwork);
@@ -38,29 +37,31 @@ function initialize() {
 
 /**
  * Cache the set of available tokens which can be used to replace artwork to avoid repeated filesystem requests
+ * Since all monsters have unique names and no duplicates with different CRs we can avoid doing a CR check for manually creating actors
  */
 async function cacheAvailableTokens() {
-	availableTokens.clear();
 	const crs = await FilePicker.browse("data", TOKEN_PATH);
 	for ( let cr of crs.dirs ) {
 		const tokens = await FilePicker.browse("data", cr+"/with-shadows/");
-		tokens.files.forEach(t => availableTokens.add(t));
+		tokens.files.forEach(t => availableTokenNames[t.split('/').slice(-1)[0].split('.')[0].toLowerCase()]  = t);
 	}
 }
 
 /**
  * Replace the artwork for a NPC actor with the version from this module
+ * Removed CR requirement, allows art to be used when making new tokens from scratch.
  */
 function replaceActorArtwork(data, options, userId) {
-	if ( !replaceArtwork || (data.type !== "npc") || !hasProperty(data, "data.details.cr") ) return;
-	const cleanName = data.name.replace(/ /g, "");
-	const crDir = String(getProperty(data, "data.details.cr")).replace(".", "-");
-	const tokenSrc = `${TOKEN_PATH}cr${crDir}/with-shadows/${cleanName}.png`;
-	if ( !availableTokens.has(tokenSrc) ) return;
+	if ( !replaceArtwork || (data.type !== "npc")) return;
+	const cleanName = data.name.replace(/ /g, "").toLowerCase();
+	if ( !cleanName in availableTokenNames ) return;
+	const tokenSrc = availableTokenNames[cleanName];
 	data.img = tokenSrc;
 	data.token = data.token || {};
 	data.token.img = tokenSrc;
-}
+};
+
+
 
 // Initialize module
 Hooks.on("init", initialize);
